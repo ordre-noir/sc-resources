250bb15599808e1c27e46b26b47553c1565406be
NargiT <3153333+NargiT@users.noreply.github.com>
2023-03-20 00:16:53 +0100

    fix postporcessing

--- fleetyards.net/postprocessing.ts
+++ fleetyards.net/postprocessing.ts
@@ -2,5 +2,5 @@ import {readJSON, writeCSV} from 'https://deno.land/x/flat@0.0.15/mod.ts'
 
 const filename = Deno.args[0]
 const arr = await readJSON(filename)
-const res = arr.map(({name, cargo}) => ({name, cargo}));
+const res = arr.map(({name, cargo}: { name: string; cargo: number }) => ({name, cargo}));
 await writeCSV(`../nargit/models.csv`, res)
