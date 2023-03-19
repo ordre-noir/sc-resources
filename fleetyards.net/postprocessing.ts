c7f9950866dc017dcfd9dcb5e4f9f8deb9b1f96b
NargiT <3153333+NargiT@users.noreply.github.com>
2023-03-20 00:49:04 +0100

    yet yet

--- fleetyards.net/postprocessing.ts
+++ fleetyards.net/postprocessing.ts
@@ -2,7 +2,7 @@ import {readJSON, writeCSV} from 'https://deno.land/x/flat@0.0.15/mod.ts'
 import {join} from 'https://deno.land/std/path/mod.ts'
 
 console.log(Deno.args)
-const filename = join("..", Deno.args[0])
+const filename = Deno.args[0]
 const arr = await readJSON(filename)
 const res = arr.map(({name, cargo}: { name: string; cargo: number }) => ({name, cargo}));
-await writeCSV(join("..", "nargit", "models.csv"), res)
+await writeCSV(join("nargit", "models.csv"), res)
