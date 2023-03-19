5746218e815e438723ac0e6c755fa3083d8ede10
NargiT <3153333+NargiT@users.noreply.github.com>
2023-03-20 00:37:41 +0100

    fleetyards

--- fleetyards.net/postprocessing.ts
+++ fleetyards.net/postprocessing.ts
@@ -1,8 +1,7 @@
 import {readJSON, writeCSV} from 'https://deno.land/x/flat@0.0.15/mod.ts'
 
+console.log(Deno.args)
 const filename = Deno.args[0]
-console.log(filename)
-console.log('Current directory: ' + process.cwd());
-const arr = await readJSON(filename)
+const arr = await readJSON(`../${filename}`)
 const res = arr.map(({name, cargo}: { name: string; cargo: number }) => ({name, cargo}));
 await writeCSV('../nargit/models.csv', res)
