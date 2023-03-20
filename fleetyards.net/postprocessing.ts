21d7a8e836e4f1078c5a847c1f1eea5cba25004c
NargiT <3153333+NargiT@users.noreply.github.com>
2023-03-20 01:00:00 +0100

    add starmap

--- fleetyards.net/postprocessing.ts
+++ fleetyards.net/postprocessing.ts
@@ -1,7 +1,6 @@
 import {readJSON, writeCSV} from 'https://deno.land/x/flat@0.0.15/mod.ts'
 import {join} from 'https://deno.land/std/path/mod.ts'
 
-console.log(Deno.args)
 const filename = Deno.args[0]
 const arr = await readJSON(filename)
 const res = arr.map(({name, cargo}: { name: string; cargo: number }) => ({name, cargo}));
